85ce71dced87255e10c01ee87e4879655cfaa932
AnRys <kokosichbdoo@gmail.com>
2026-01-03 18:12:36 +0300

    main

--- src/main/java/com/fishing/FishingGame/Services/FishGenerator.java
+++ src/main/java/com/fishing/FishingGame/Services/FishGenerator.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public final class FishGenerator implements IGenerator {
     private final Random random = new Random();
     private final Double[] chances = {2.0,50.0,100.0,130.0};
-    private final Double[] normilizedChances = LuckService.normalizeChances();
+    //private final Double[] normilizedChances = LuckService.normalizeChances();
     private final Map<FISHRARITY, Double> rarityWeights = Map.of(
             FISHRARITY.COMMON, 0.5,
             FISHRARITY.UNCOMMON, 0.3,
